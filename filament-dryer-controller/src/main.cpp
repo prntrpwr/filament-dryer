390cf91bd564b95b545ea24a429f5787b8b19e67
jffhff <jffhff+github@thehoffmanns.net>
2024-09-18 21:05:25 -0400

    RAMPS doesn't have a second heater.  Code working for fans/heaters

--- filament-dryer-controller/src/main.cpp
+++ filament-dryer-controller/src/main.cpp
@@ -2,6 +2,12 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
+#define COMMAND_LINE_MAX_LENGTH_CHARS 16
+
+#define HEATER_2_PIN 8
+#define FAN_1_PIN 10
+#define FAN_2_PIN 9
+
 LiquidCrystal_I2C lcd(0x27,20,4);  // LCD I2C address is 0x27 and it is a 16x2 char display
 
 unsigned int led_on = 0;
@@ -14,11 +20,19 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   led_blink();
 
+  pinMode(FAN_1_PIN, OUTPUT);
+  pinMode(FAN_2_PIN, OUTPUT);
+  pinMode(HEATER_2_PIN, OUTPUT);
+
   Serial.begin(115200);
 
   lcd.init();
   lcd.backlight();
 }
+
+uint8_t fan_1_pwm_duty_cycle_255 = 0;
+uint8_t fan_2_pwm_duty_cycle_255 = 0;
+uint8_t heater_2_pwm_duty_cycle_255 = 0;
 
 unsigned long last_interval_timestamp_millis = 0;
 void lcd_display() {
@@ -26,26 +40,45 @@ void lcd_display() {
 
   /* 0,0 is first character of first row, 0,1 is first character of 2nd row*/
   lcd.setCursor(0,0);
-  lcd.print(led_on);
+  lcd.print((last_interval_timestamp_millis / 1000) % 10000);
 
-  lcd.setCursor(0,1);
-  lcd.print(last_interval_timestamp_millis);
+  lcd.setCursor(5,0);
+  lcd.print("1F");
+  lcd.print(fan_1_pwm_duty_cycle_255);
+  lcd.setCursor(11,0);
+  lcd.print("2F");
+  lcd.print(fan_2_pwm_duty_cycle_255);
+
+  lcd.setCursor(5,1);
+  lcd.print("1H");
+  lcd.print(0);
+  lcd.setCursor(11,1);
+  lcd.print("2H");
+  lcd.print(heater_2_pwm_duty_cycle_255);
 }
 
 void set_fan(int fan_num, int fan_duty_cycle) {
-  Serial.print("Set fan ");
-  Serial.print(fan_num);
-  Serial.print(" to duty cycle ");
-  Serial.print(fan_duty_cycle);
-  Serial.println();
+  if (fan_num==1) {
+    fan_1_pwm_duty_cycle_255 = fan_duty_cycle;
+    analogWrite(FAN_1_PIN, fan_1_pwm_duty_cycle_255);
+    Serial.println("OK");
+  } else if (fan_num==2) {
+    fan_2_pwm_duty_cycle_255 = fan_duty_cycle;
+    analogWrite(FAN_2_PIN, fan_2_pwm_duty_cycle_255);
+    Serial.println("OK");
+  } else {
+    Serial.println("E Invalid fan number");
+  }
 }
 
 void set_heater(int heater_num, int heater_duty_cycle) {
-  Serial.print("Set heater ");
-  Serial.print(heater_num);
-  Serial.print(" to duty cycle ");
-  Serial.print(heater_duty_cycle);
-  Serial.println();
+  if (heater_num==2) {
+    heater_2_pwm_duty_cycle_255 = heater_duty_cycle;
+    analogWrite(HEATER_2_PIN, heater_2_pwm_duty_cycle_255);
+    Serial.println("OK");
+  } else {
+    Serial.println("E Invalid heater number");
+  }
 }
 
 void start_measurement_interval(int interval_duration_ms) {
@@ -89,7 +122,12 @@ void loop() {
       process_command_line(serial_in);
       serial_in = "";
     } else {
-      serial_in += c;
+      if (serial_in.length() > COMMAND_LINE_MAX_LENGTH_CHARS) {
+        Serial.println("E Command line length limit exceeded");
+        serial_in = "";
+      } else {
+        serial_in += c;
+      }
     }
   }
 
