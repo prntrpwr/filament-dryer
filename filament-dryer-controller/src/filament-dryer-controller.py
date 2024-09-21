e43ae6c91baf66f402d2756f8ce37aebacfbb9d6
jffhff <jffhff+github@thehoffmanns.net>
2024-09-21 00:29:47 -0400

    Dynamic equilibration. First tests

--- filament-dryer-controller/src/filament-dryer-controller.py
+++ filament-dryer-controller/src/filament-dryer-controller.py
@@ -26,10 +26,36 @@ def serial_cmd(cmd):
     #print(response)
     return response
 
-def equilibrate():
-    # TODO
-    time.sleep(2*interval_duration)
-    time.sleep(1)
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+    
+def values_within(readingsa, readingsb, allowed_diff):
+    if readingsa is None or readingsb is None: return False
+    if len(readingsa) != len(readingsb): return False
+
+    for i in range(9,len(readingsa)):
+        if not is_number(readingsa[i]): continue
+        a = float(readingsa[i])
+        b = float(readingsb[i])
+        diff = min(a, b) / max(a, b)
+        if (diff < allowed_diff) :
+            #print(a,b,diff)
+            return False
+
+    return True
+
+def equilibrate(min_s=10, max_s=60, interval_s=10, min_drift=0.999):
+    sensor_readings = None
+    last_sensor_readings = None
+    start = time.time()
+    while ((not values_within(sensor_readings, last_sensor_readings, min_drift)) and ((time.time() - start) < max_s)) or ((time.time() - start) < min_s):
+        last_sensor_readings = sensor_readings
+        sensor_readings = serial_cmd(f"R {interval_s}").split(",")
+        #print(sensor_readings)
 
 def record_sensors():
     interval_start = round(time.time())
@@ -66,17 +92,17 @@ def test_hot_step():
     everything_off()
 
     # fan 1 on 111, fan 2 on 132, equilibrate
-    serial_cmd("F 1 111")
-    serial_cmd("F 2 132")
-    equilibrate()
+    serial_cmd("F 1 255")
+    serial_cmd("F 2 255")
+    equilibrate(min_s=600,max_s=900)
+    record_sensors()
 
     # for each heater power from 255 to 0
-    for heater_pwm in range(255, -1, -5):
+    # TODO up and down
+    for heater_pwm in range(15, -1, -1):
         # heater 2 on power, equilibrate
         serial_cmd(f"H 2 {heater_pwm}")
-        equilibrate()
-
-        # record
+        equilibrate(min_s=600,max_s=900)
         record_sensors()
 
     serial_cmd("D Complete")
@@ -84,4 +110,44 @@ def test_hot_step():
 
     serial_end()
 
-test_fan_step_pwm_pressure(5)
+def test_hot_fan_step(step_size=25, heater_pwm=15, start_fan_pwm=125):
+    serial_start()
+    serial_cmd("D hotfanstep")
+    everything_off()
+
+    fan_pwm_list = list(range(start_fan_pwm,255, 1*step_size))
+    midpoint_between_last_two = round((fan_pwm_list[-1] + fan_pwm_list[-2])/2)
+    fan_pwm_list += list(range(midpoint_between_last_two,start_fan_pwm, -1*step_size))
+    print(fan_pwm_list)
+
+    serial_cmd("F 1 255")
+    serial_cmd("F 2 255")
+    equilibrate(min_s=600,max_s=900)
+    record_sensors()
+
+    for fan_pwm in fan_pwm_list:
+        serial_cmd(f"F 1 {fan_pwm}")
+        serial_cmd("F 2 255")
+        serial_cmd(f"H 2 {heater_pwm}")
+        equilibrate(min_s=600,max_s=900)
+        record_sensors()
+
+    for fan_pwm in fan_pwm_list:
+        serial_cmd("F 1 255")
+        serial_cmd(f"F 2 {fan_pwm}")
+        serial_cmd(f"H 2 {heater_pwm}")
+        equilibrate(min_s=600,max_s=900)
+        record_sensors()
+
+    serial_cmd("H 2 0")
+    serial_cmd("F 1 255")
+    serial_cmd("F 2 255")
+    equilibrate(min_s=600,max_s=900)
+    record_sensors()
+
+    serial_cmd("D Complete")
+    everything_off()
+
+    serial_end()
+
+test_hot_fan_step(step_size=50, heater_pwm=15, start_fan_pwm=100) # 2024-09-20
